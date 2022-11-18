48597a3b25f2a35b9b1a5121a4038bafc4099aef
Sibusiso Labi <49154656+CarnoSkyla@users.noreply.github.com>
2022-11-18 16:47:32 +0200

    Set up socket.io

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -19,9 +23,14 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('user connected');
+})
+
+var server = http.listen(3000, () => {
     console.log('server is running on port ' + server.address().port);
 }) 
